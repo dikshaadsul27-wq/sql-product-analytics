ed94f1bf054b9e5761dc3d0f83e4d6caabd1c2e8
dikshaadsul27-wq <dikshaadsul27@gmail.com>
2026-07-15 16:41:04 +0530

    Update Query E3 — Cohort Retention Curve (Weekly, Behavioral).sql

--- queries/Query E3 — Cohort Retention Curve (Weekly, Behavioral).sql
+++ queries/Query E3 — Cohort Retention Curve (Weekly, Behavioral).sql
@@ -5,15 +5,17 @@
 -- Sanity check: cohort size = week0 active customers
 
 with signup_cohorts as (
-    select
-        c.customer_id
-        , date_trunc('week', c.created_at) as cohort_week
-        , c.created_at as signup_time
+ select 
+        c.customer_id,
+        least(min(se.occurred_at),c.created_at) as signup_time,
+		date_trunc('week',least(min(se.occurred_at),c.created_at)) as cohort_week
     from ecom.customers c
-    where c.created_at >= '2026-04-19'
-)
-
-, retention as (
+	left join ecom.session_events se
+    on c.customer_id = se.customer_id
+    where c.created_at >= '2026-04-19' -- exclude uninstrumented cohorts
+	group by c.customer_id
+),
+retention as (
     select
         sc.cohort_week
         , sc.customer_id
@@ -33,7 +35,13 @@ select
     , count(distinct case when week_index = 2 then sc.customer_id end) as w2_retained
     , count(distinct case when week_index = 3 then sc.customer_id end) as w3_retained
     , count(distinct case when week_index = 4 then sc.customer_id end) as w4_retained
+	, ROUND(count(distinct case when week_index = 1 then sc.customer_id end)::numeric/count(distinct sc.customer_id),2) as w1_retention_rate
+	, ROUND(count(distinct case when week_index = 2 then sc.customer_id end)::numeric/count(distinct sc.customer_id),2) as w2_retention_rate
+	, ROUND(count(distinct case when week_index = 3 then sc.customer_id end)::numeric/count(distinct sc.customer_id),2) as w3_retention_rate
+	, ROUND(count(distinct case when week_index = 4 then sc.customer_id end)::numeric/count(distinct sc.customer_id),2) as w4_retention_rate
+	
 from signup_cohorts sc
 left join retention r on sc.customer_id = r.customer_id
 group by sc.cohort_week
 order by sc.cohort_week;
+
